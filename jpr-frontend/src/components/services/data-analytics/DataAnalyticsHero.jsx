3e169deeed624238ad2463fcf02cc6b776956cb0
Neeraj Gavde <intern@applauseitsolutions.com>
2026-01-09 16:05:41 +0530

    changes

--- jpr-frontend/src/components/services/data-analytics/DataAnalyticsHero.jsx
+++ jpr-frontend/src/components/services/data-analytics/DataAnalyticsHero.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import heroImg from "../../../assets/images/services/data-hero.jpg";
+import heroImg from "../../../assets/images/services/data-hero.png";
 import logo from "../../../assets/logos/logo.png";
 import logo2 from "../../../assets/logos/rassi.png";
 
